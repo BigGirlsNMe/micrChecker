c560dab1ee936d444e2e1c10646f46e2886608bb
BigGirlsNMe <34071944+BigGirlsNMe@users.noreply.github.com>
2019-04-15 21:06:44 -0700

    fixed ui elements
    correctly packed ui elements with tkinter
    added buttonframe and canvas frame widgets
    TODO: save functionality
    resize check image in display

--- micrChecker.py
+++ micrChecker.py
@@ -22,21 +22,23 @@ import sys
 # create delimeters with different parameters depending on size of check
 # variables set for business check
 
-
-## change image to rgb type 8 bit add color dimension to draw lines on check image
+ogImgGlobal = None
+ogImgAltered = None
+# (0,0) coordinate in imagedraw begins in top left corner of image. since ansi x9 standards place amount status delimeters from right edge of check, we subtract the difference of the standars from the width of the check to get the appropriate placement of our delimeter
 
 def checkSize(img):
-	print("\nChecksize method")
 	if img.width > 1205: # check if business check 
+		print("Check size is determined to be business check")
 		routingFieldDelStart = img.width - 1150 
 		routingFieldDelEnd = img.width - 850 
 		onUsFieldDelStart = img.width - 850
 		onUSFieldDelEnd = img.width - 375
 	
 #==================================================================
-#defaults to personal check if check size not big enough for business	
+#defaults to personal check if check size not big enough for business
+#TODO: change if 	
 	else:
-		print('toosmallforbiz') #default to personal if small size
+		print('Check size is determined to be personal check') #default to personal if small size
 		routingFieldDelStart = img.width - 950#bad numbers all in her for personal check size  
 		routingFieldDelEnd = img.width - 650 
 		onUsFieldDelStart = img.width - 650
@@ -49,121 +51,119 @@ def checkSize(img):
 	drawLines(onUSFieldDelEnd, img)
 	drawLines(onUsFieldDelStart, img)
 	img.save("Test.jpeg","jpeg")
-	#img.show()
 	return img
 #==============================================================================
 #Draw lines handled in this method
 def drawLines(linePos, img):
-	print("drawing lines on image here")
-	#============================================
-	#test
-	#prob cannot convert here
-	#imgRgb = img.convert("RGB")
-	#==============================================
+	print "drawing line on image at",linePos,"pixels"
 	#open new imagedraw library for image manipulate on img variable passed to method
 	#=======================================
 	draw = ImageDraw.Draw(img)
 	#=======================================
-	#drawrgb = ImageDraw.Draw(imgRgb)
 	#Parameters for Drawline
 	#(xy coordninates of start, xy coordinates of end), ink, width
 	#PIL.ImageDraw.ImageDraw.line(xy, fill=None, width=0, joint=None)
-	#draw.line((linePos, 0,linePos,img.height),128,width=5)
 	#====================================================
-	draw.line((linePos, 0,linePos,img.height),(0),width=2)
-	#drawrgb.line((linePos, 0,linePos,imgRgb.height),fill=(255,0,0),width=2)
+	#green color lines
+	draw.line((linePos, 0,linePos,img.height),(0,128,0),width=2)
 	#======================================================
 	#draw.text((linePos+2),0, img.filename)
 	return
+	
+def convertToRGB(img):
+		imgRgb = img.convert("RGB")
+		return imgRgb
+		
+		
+		
+		
+#TODO need to fill out this method		does not work!!!
+def saveImage():
+	fout = tkFileDialog.asksaveasfile(mode='w', defaultextension=".tiff")
+	text2save = str(self.text.get(0.0,END))
+	fout.write(text2save)
+	fout.close()
+		
+	if f is None: # asksaveasfile return None if dialog is closed with cancel
+		return
+	
 #===============================================================================
 # Creating Initializing UI components like title and app size
 top = Tkinter.Tk()
 top.title("micrChecker")
-top.geometry('600x750')
-canvasTop = Tkinter.Canvas(top, width=500, height=300)
-canvasBtm = Tkinter.Canvas(top, width=500, height=300)
+#top.geometry('600x750')
+buttonFrame = Tkinter.Frame(top)
+canvasFrame = Tkinter.Frame(top)
+#=========================
+
 #=============================================================================
 #open file explorer to select best binarized image from file explorer
 def openFileExplore():
 	file = tkFileDialog.askopenfile(parent=top,mode='rb',title='Upload BestBinarized Front')
 	if file != None:
 		img = Image.open(file)
-		print(img.format + " image inside openFIleExplore")
+		print"***Original Images Information***\nFormat of image = " ,img.format , "\nMode of Image = " , img.mode , "\nDPI = ", img.info['dpi']
+		#==============================================
+		#displays image on cavas and sets image open to global variable		
 		displayOnCanvas(img)
-		#=================
-		imgRgb = img.convert("RGB")
-		#=================
-		newImg = checkSize(imgRgb)
-		#=========================
-		print("New Image inside openFile ")
-		displayOnBottom(newImg)
-		#
-		#get img ready for processing in here
-		#
+		global ogImgGlobal
+		ogImgGlobal = img
+		#=========== sample returning for test
 		file.close()
-		#print "i got %d bytes from this file " % len(data)
-		print "Exiting openFileExplore" + img.format
-		top.mainloop()
+		return 
 		
-#========================================================
+		
+#============================================================
 #display to BottomSLopply redo
 def displayOnBottom(img):
-	print("This is inside diplay Bottom")
+	print"Displaying altered image to bottom canvas"
 	imgSize = img.resize((425,175),Image.ANTIALIAS)
-	print(imgSize.width)
+	print"Resizing image for ui width = ", imgSize.width,", height = ", imgSize.height
 	tkpi = ImageTk.PhotoImage(imgSize)
 	canvasBtm.create_image(250,175,image=tkpi)
 	canvasBtm.image = tkpi # required or will be garbage collected
 	return
-	
-#======================================================	
-		
-#========================================================
+#=============================================================
 #display to Canvas
 def displayOnCanvas(img):
-	print("This is inside diplay on Canvas")
+	print"Displaying Original Image to top Canvas"
 	imgSize = img.resize((425,175),Image.ANTIALIAS)
-	print("Image width after resize" )
+	print"Resizing image for ui width = ", imgSize.width,", height = ", imgSize.height
 	tkpi = ImageTk.PhotoImage(imgSize)
 	canvasTop.create_image(250,175,image=tkpi)
 	canvasTop.image = tkpi
 	return
 	
 #======================================================	
+# does the work for drawing lines on button calls our other methods. 
+def manipulateImage():
+	imgRgb = convertToRGB(ogImgGlobal)
+	drawnOnImg = checkSize(imgRgb)
+	print"***Altered Image information***\nFormat of image = " ,imgRgb.format , "\nMode of Image = " , imgRgb.mode , "\nDPI = ", imgRgb.info['dpi']
+	displayOnBottom(drawnOnImg)
+#=========================================================
 #pack components into ui
-
-BUpload = Tkinter.Button (top, text = 'Upload BestBinarized Front', command = openFileExplore)
-#BConvert = Tkinter.Button(top, text = 'Draw delimeters', command= displayOnCanvas)
+#working in here
+canvasTop = Tkinter.Canvas(canvasFrame, width=500, height=300)
+canvasBtm = Tkinter.Canvas(canvasFrame, width=500, height=300)
+BUpload = Tkinter.Button (buttonFrame, text = 'Upload bestbinarized front', command = openFileExplore)
+BConvert = Tkinter.Button(buttonFrame, text = 'Draw Delimeters', command= manipulateImage)
 #BHelp = TkinterButton(top,text='Help",command = openHelp)
-BUpload.pack(padx=10, pady=10)
-canvasTop.pack(padx=10,pady=10)
-canvasBtm.pack(padx=10,pady=10)
-#BConvert.pack(padx=10, pady=10)
+BSave = Tkinter.Button(buttonFrame,text='Save image',command = saveImage)
+
+#=============================================================================
+
+buttonFrame.pack(side='top',)
+canvasFrame.pack(side='bottom',fill='y')
+BUpload.pack(side='left',fill='x',padx=10)
+BConvert.pack(side='left',padx=10)
+BSave.pack(side='left',padx=10)
+canvasTop.pack()
+canvasBtm.pack()
+
 
 #=============================================================
 
 top.mainloop() #makes menu and does nothing until you interact with items
 
-sys.exit("Stopped")
-#check image this needs to be in the same folder as this script
-frontCheckImg = openFileExplore
-
-#====================================================
-#frontCheckImg = 'mdtestimg.tiff'
-# assign image to new variable for manipulation
-
-#open image	and print some image details
-img = Image.open(frontCheckImg)
-print("Format of image = " + img.format + "\nMode of Image = " + img.mode + "\nDPI = ")
-print(img.info['dpi'])
-
-# (0,0) coordinate in imagedraw begins in top left corner of image. since ansi x9 standards place amount status delimeters from right edge of check, we subtract the difference of the standars from the width of the check to get the appropriate placement of our delimeter
-
-
-	
-#========================================================================
-#shows image produced and saves it 	
-#show the image with default os imageviewer
-img.show()
-#save the new image to jpeg called DrawnOn
-img.save('DrawnOn.jpeg' ,'jpeg')
+sys.exit("Stopping program")
